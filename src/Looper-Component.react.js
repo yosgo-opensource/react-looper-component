a86e4dfd552cc43fb5e9a65c7319734ae4fa97d5
Whien Liou <sal95610@gmail.com>
2017-09-28 06:24:43 +0800

    PropTypes and Support object type

--- src/Looper-Component.react.js
+++ src/Looper-Component.react.js
@@ -1,20 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const isFiber = React.version.indexOf('16') > -1;
+const isObject = items => typeof items === 'object';
 
 const LooperComponent = ({ items, render }) => {
-  if (items === null) return null;
+  if (items === null || render === null) return null;
+
+  let child;
 
   if (items.length) {
-    const child = items.map((item, index) => render(item, index));
-    if (isFiber) {
-      return child;
-    } else {
-      return <span>{child}</span>;
-    }
+    child = items.map((item, index) => render(item, index));
   }
 
-  return null;
+  if (isObject(items)) {
+    const keys = Object.keys(items);
+    child = keys.map((key, index) => render(items[key], index));
+  }
+
+  if (isFiber) {
+    return child;
+  } else {
+    return <span>{child}</span>;
+  }
+};
+
+LooperComponent.propTypes = {
+  items: PropTypes.oneOfType([PropTypes.array, PropTypes.object]).isRequired,
+  reder: PropTypes.func
+};
+LooperComponent.defaultProps = {
+  items: [],
+  render: null
 };
 
 export default LooperComponent;
